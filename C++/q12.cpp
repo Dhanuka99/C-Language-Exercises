f25159b0058b5155a1fb8a16600e975db5adb989
DHANUKA <danuka1999lakshan@gmail.com>
2022-03-29 00:05:02 +0530

    example

---
+++ C++/q12.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<cmath>
+#include<iomanip>
+using namespace std;
+
+
+int main(){
+	string name;
+	cout<<sqrt(2)<<endl;
+	cout.precision(2);
+	cout<<fixed;
+	cout<<sqrt(2);
+	return 0;
+}
---
+++ C++/q13.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<cmath>
+#include<iomanip>
+using namespace std;
+class Test{
+	int x;
+	
+};
+
+int main(){
+Test t;
+
+	cout<<t.x;
+	return 0;
+}
